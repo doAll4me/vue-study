5d950b6ab35c8cebb35a17b73bdb3d480a6a9596
ee <1176187361@qq.com>
2026-01-09 20:54:21 +0800

    '0109route路由学习'

---
+++ 备份/12_routes_query/views/Detail.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="news-list">
+    <!-- query是响应式数据 所以页面会随着他的更新而更新 -->
+    <!-- <li>编号：{{ route.query.id }}</li>
+    <li>标题：{{ route.query.title }}</li>
+    <li>内容：{{ route.query.content }}</li> -->
+    <li>编号：{{ query.id }}</li>
+    <li>标题：{{ query.title }}</li>
+    <li>内容：{{ query.content }}</li>
+  </ul>
+</template>
+
+<script setup lang="ts" name="About">
+import { toRefs } from "vue";
+import { useRoute } from "vue-router";
+let route = useRoute();
+// console.log(route.query);
+let { query } = toRefs(route); //对一个响应式对象的属性进行解构赋值时，要使用 toRefs 或 toRef 来保持响应性。
+</script>
+
+<style scoped>
+.news-list {
+  list-style: none;
+  padding-left: 20px;
+}
+
+.news-list > li {
+  line-height: 30px;
+}
+</style>
---
+++ 备份/14_routes_props/router/index.ts
@@ -0,0 +1,53 @@
+// 路由器
+import { createRouter, createWebHistory } from "vue-router"; //1.创建路由器
+//2.引入组件
+import Home from "@/views/Home.vue";
+import News from "@/views/News.vue";
+import About from "@/views/About.vue";
+import Detail from "@/views/Detail.vue";
+
+const router = createRouter({
+  history: createWebHistory(), //3.路由器工作模式（vue3必不可少
+  routes: [
+    //4.管理一个个路由规则
+    {
+      name: "zhuye",
+      path: "/home",
+      component: Home,
+    },
+    {
+      name: "xinwen",
+      path: "/news",
+      component: News,
+      children: [
+        {
+          name: "xiangqing", //params一定要name
+          path: "detail/:id/:title/:content", //params参数需要提前在路由设置中占位
+          component: Detail,
+
+          //第一种写法:将路由收到的所有params参数作为props传给路由组件
+          // props: true, //路由的props配置
+
+          // 第二种写法--函数写法:可以自己决定将什么作为props参数
+          props(route) {
+            return route.query;
+          },
+
+          // 第三种写法--对象写法:可以自己决定将什么作为props参数，但是是写死的
+          // props: {
+          //   a: 100,
+          //   b: 200,
+          //   c: 300,
+          // },
+        },
+      ],
+    },
+    {
+      name: "guanyu",
+      path: "/about",
+      component: About,
+    },
+  ],
+});
+
+export default router;
